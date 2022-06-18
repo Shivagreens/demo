96a2533c7340ed4262cc6391a1f42082397e7e6c
Shivagreens <shivagreensomr@gmail.com>
2022-06-18 16:00:23 +0530

    new commens

---
+++ src/org/phones/Iphones.java
@@ -0,0 +1,31 @@
+package org.phones;
+
+//          class name
+public class Iphones {
+
+	// method creation
+	private void iPad() {
+
+		//to print some value
+		System.out.println("the best ipad is ipad pro 2021");
+	}
+	
+	private void iPhone() {
+        
+		System.out.println("the latest iphone is 13 pro max");
+
+	}
+	
+	//main method
+	public static void main(String[] args) {
+		//object creation
+		Iphones i = new Iphones();
+		
+		//method calling
+		i.iPad();
+		i.iPhone();
+		
+	}
+	
+	
+}
